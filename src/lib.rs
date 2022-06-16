2322b3c1afd48a92bbf12b85c9291f174e34bece
Noe Huaccharaque <noemk2@gmail.com>
2022-06-16 00:22:52 -0500

    div_modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,4 @@
-use clap::Parser;
-
-#[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)] //  is optional
-pub struct Args {
-    /// Name of the person to greet
-    #[clap(short, long, value_parser)]
-    pub name: String,
-
-    /// Number of times to greet
-    #[clap(short, long, value_parser, default_value_t = 1)]
-    pub count: u8,
-}
-
-pub fn get_args() {
-    let args = Args::parse();
-
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name)
-    }
+pub mod views {
+    // pub mod cli_default;
+    pub mod cli_positional;
 }
