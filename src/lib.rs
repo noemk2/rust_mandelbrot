9fff4916cd916408f1b9ed692f08581c9157c6a9
Noe Huaccharaque <noemk2@gmail.com>
2022-06-16 18:24:20 -0500

    mod_controller

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,15 @@
 pub mod views {
-    pub mod default;
-    pub mod positional;
+    // pub mod default;
+    // pub mod positional;
     pub mod quick_derive;
-    pub mod settings_parser;
+    // pub mod settings_parser;
 }
 
 pub mod images {
     pub mod fractal;
     pub mod mandelbrot;
+}
+
+pub mod controller {
+    pub mod control;
 }
--- src/views/quick_derive.rs
+++ src/views/quick_derive.rs
@@ -4,35 +4,39 @@ use std::path::PathBuf;
 
 use clap::{Parser, Subcommand};
 
-use super::super::images::{
-    // fractal::fractal,
-    mandelbrot::mandelbrot,
-    // fractal::saludo,
-    // mandelbrot::saludo,
-};
+// use super::super::images::{
+// fractal::fractal,
+// mandelbrot::mandelbrot,
+// fractal::saludo,
+// mandelbrot::saludo,
+// };
+
+// use crate::controller::control::controller_fn;
+
+// use super::super::controller::control;
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
-struct Cli {
+pub struct Cli {
     /// Optional name to operate on
     #[clap(value_parser)]
-    name: Option<String>,
+    pub name: Option<String>,
 
     /// Sets a custom config file
     #[clap(short, long, value_parser, value_name = "mandelbrot.png")]
     // config: Option<PathBuf>,
-    out: Option<PathBuf>,
+    pub out: Option<PathBuf>,
 
     /// Turn debugging information on
     #[clap(short, long, action = clap::ArgAction::Count)]
-    debug: u8,
+    pub debug: u8,
 
     #[clap(subcommand)]
     command: Option<Commands>,
 }
 
 #[derive(Subcommand)]
-enum Commands {
+pub enum Commands {
     /// does testing things
     Test {
         /// lists test values
@@ -41,51 +45,47 @@ enum Commands {
     },
 }
 
-pub fn cli_quick() {
+pub fn cli_quick() -> Cli {
     let cli = Cli::parse();
+    return cli;
 
-    // You can check the value provided by positional arguments, or option arguments
-    if let Some(name) = cli.name.as_deref() {
-        // // (?i)\.(jpg|png)$
-        // let re = Regex::new(r"(?i)\.(jpg|png)$").unwrap();
-        // let caps = re.captures(name).unwrap();
-        if  name.ends_with( ".jpg" ) || name.ends_with( ".png" ) {
-            // println!("{}", name);
-            // fractal(&name)
-            mandelbrot(&name);
-        }
-        else {
-            mandelbrot(&vec![name, ".png"].concat());
-        }
-
-        // fractal();
-    }
-
-    if let Some(config_path) = cli.out.as_deref() {
-        println!("Value for output: {}", config_path.display());
-    }
-
-    // You can see how many times a particular flag or argument occurred
-    // Note, only flags can have multiple occurrences
-    match cli.debug {
-        0 => println!("Debug mode is off"),
-        1 => println!("Debug mode is kind of on"),
-        2 => println!("Debug mode is on"),
-        _ => println!("Don't be crazy"),
-    }
-
-    // You can check for the existence of subcommands, and if found use their
-    // matches just as you would the top level cmd
-    match &cli.command {
-        Some(Commands::Test { list }) => {
-            if *list {
-                println!("Printing testing lists...");
-            } else {
-                println!("Not printing testing lists...");
-            }
-        }
-        None => {}
-    }
-
-    // Continued program logic goes here...
 }
+
+
+// pub fn cli_quick() {
+//     let cli = Cli::parse();
+
+//     // You can check the value provided by positional arguments, or option arguments
+//     if let Some(name) = cli.name.as_deref() {
+//         //   controller_fn(name);
+//         println!("name: {:?}", name);
+//     }
+
+//     if let Some(config_path) = cli.out.as_deref() {
+//         println!("Value for output: {}", config_path.display());
+//     }
+
+//     // You can see how many times a particular flag or argument occurred
+//     // Note, only flags can have multiple occurrences
+//     match cli.debug {
+//         0 => println!("Debug mode is off"),
+//         1 => println!("Debug mode is kind of on"),
+//         2 => println!("Debug mode is on"),
+//         _ => println!("Don't be crazy"),
+//     }
+
+//     // You can check for the existence of subcommands, and if found use their
+//     // matches just as you would the top level cmd
+//     match &cli.command {
+//         Some(Commands::Test { list }) => {
+//             if *list {
+//                 println!("Printing testing lists...");
+//             } else {
+//                 println!("Not printing testing lists...");
+//             }
+//         }
+//         None => {}
+//     }
+
+//     // Continued program logic goes here...
+// }
